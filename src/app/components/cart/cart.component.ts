87edcae29fa0e29b1963ae4ad4884ea851bbaa00
Le Nam <ldnam806@gmail.com>
2022-12-06 23:50:26 +0700

    update cart theme and form checkout with validation

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { ProductInCart } from 'src/app/models/productInCart';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'app-list',
-    templateUrl: './cart.component.html',
-    styleUrls: ['./cart.component.scss']
-  })
-  export class CartComponent implements OnInit {
-    constructor() {}
-    ngOnInit(): void {
-    }
-
+  selector: 'app-list',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss'],
+})
+export class CartComponent {
+  carts: ProductInCart[] = [];
+  total: number = 0;
+  angForm!: FormGroup;
+  constructor(private productService: ProductService, private fb: FormBuilder) {
+    this.createForm();
   }
-
-   
+  async ngOnInit() {
+    this.carts = await this.productService.getMyCart();
+    this.total = await this.productService.sumTotal();
+  }
+  createForm() {
+    this.angForm = this.fb.group({
+      name: ['', Validators.required],
+      address: ['', Validators.required],
+      'card-number': ['', Validators.required],
+    });
+  }
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,47 +6,57 @@ import { User } from '../models/user';
 import { ProductInCart } from '../models/productInCart';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class ProductService {
-    myCart: ProductInCart[] = [];
-    user: User = {
-        totalPrice: 0,
-        fullName: '',
-        address: '',
-        creditNumber: '',
+  myCart: ProductInCart[] = [];
+  user: User = {
+    totalPrice: 0,
+    fullName: '',
+    address: '',
+    creditNumber: '',
+  };
+  private personURL = 'http://localhost:3000/products';
+  constructor(private http: HttpClient) {}
+
+  getProductList(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.personURL);
+  }
+  getUser() {
+    return this.user;
+  }
+
+  getProductById(id: number): Observable<Product[]> {
+    const params = {
+      id,
     };
-    private personURL = 'http://localhost:3000/products';
-    constructor(private http: HttpClient) { }
-
-    getProductList(): Observable<Product[]> {
-        return this.http.get<Product[]>(this.personURL);
+    return this.http.get<Product[]>(this.personURL, { params });
+  }
+  getMyCart() {
+    return this.myCart;
+  }
+  addToCart(product: Product) {
+    var elementPos = this.myCart
+      .map(function (x: any) {
+        return x.id;
+      })
+      .indexOf(product.id);
+    var objectFound = this.myCart[elementPos];
+    if (objectFound) {
+      this.myCart[elementPos].quantity++;
+    } else {
+      this.myCart.push({ ...product, quantity: 1 });
     }
-    getUser() {
-        return this.user;
-    }
-
-    getProductById(id: number): Observable<Product[]> {
-        const params = {
-            id,
-        };
-        return this.http.get<Product[]>(this.personURL, { params });
-    }
-    getMyCart() {
-        return this.myCart;
-    }
-    addToCart(product: Product) {
-        var elementPos = this.myCart.map(function(x:any) {return x.id; }).indexOf(product.id);
-        var objectFound = this.myCart[elementPos];
-        if(objectFound){
-            this.myCart[elementPos].quantity ++
-        }else{
-            this.myCart.push({ ...product, 'quantity': 1 })
-        }
-    
-    }
-    remove(id: number) {
-        // const index = this.myCart.findIndex((i: Product) => i.id === id);
-    }
-
+  }
+  sumTotal() {
+    const total = this.myCart.reduce(
+      (accumulator, currentValue) =>
+        accumulator + currentValue.price * currentValue.quantity,
+      0
+    );
+    return Number(total.toFixed(1));
+  }
+  remove(id: number) {
+    // const index = this.myCart.findIndex((i: Product) => i.id === id);
+  }
 }
